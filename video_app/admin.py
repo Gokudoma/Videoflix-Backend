323da1c7f3f9fcd20d1f62cdc2dcd84e32686125
Gokudoma <stephan_wacker@hotmail.com>
2026-02-11 15:52:01 +0100

    feat: make video_file mandatory and disable clear option in admin

--- video_app/admin.py
+++ video_app/admin.py
@@ -3,4 +3,15 @@ from .models import Video
 
 @admin.register(Video)
 class VideoAdmin(admin.ModelAdmin):
+    """
+    Admin interface configuration for the Video model.
+    """
     list_display = ('id', 'title', 'created_at')
+
+    def get_form(self, request, obj=None, **kwargs):
+        """
+        Customizes the form to ensure video_file is mandatory and cannot be cleared.
+        """
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields['video_file'].required = True
+        return form
--- video_app/models.py
+++ video_app/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.utils import timezone
 
 class Video(models.Model):
+    """
+    Model representing a video entity with its metadata and associated source files.
+    """
     CATEGORY_CHOICES = [
         ('drama', 'Drama'),
         ('documentary', 'Documentary'),
@@ -12,9 +15,9 @@ class Video(models.Model):
     title = models.CharField(max_length=80)
     description = models.TextField(max_length=500)
     created_at = models.DateTimeField(default=timezone.now)
-    video_file = models.FileField(upload_to='videos', blank=True, null=True)
+    video_file = models.FileField(upload_to='videos')
     thumbnail = models.FileField(upload_to='thumbnails', blank=True, null=True)
-    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='drama') 
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='drama')
 
     def __str__(self):
         return self.title
